4e809368160c0cb218605a6c5ffa20d364f06622
Brooks Patton <brooks.patton@galvanize.com>
2017-07-24 14:13:08 -0600

    added a test

---
+++ tests/catnames.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const supertest = require('supertest');
+const app = require('../app');
+
+const api = supertest(app);
+const should = chai.should();
+
+describe('catnames api', () => {
+	it('should return all cats', (done) => {
+		api.get('/catnames')
+		.expect(200)
+		.end((err, res) => {
+			if(err) return done(err);
+
+			res.body[0].name.should.be.a('string');
+
+			done();
+		});
+	});
+
+	it('should add a cat name to the db', (done) => {
+		api.post('/catnames')
+		.send({name: 'myAwesomeCatName'})
+		.expect(200, done);
+	});
+});
